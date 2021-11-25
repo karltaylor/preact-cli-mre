fc1366230d1911483396710b57c50263e3721de8
Karl Taylor <karl@karltaylor.dev>
2021-11-25 15:29:29 +0000

    Create error example

---
+++ preact.config.js
@@ -0,0 +1,12 @@
+export default (config, env, helpers) => {
+  /**
+   * Uncomment below to fix build error.
+   */
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     react: "preact/compat",
+  //     "react-dom": "preact/compat",
+  //   };
+
+  console.log(config);
+};
